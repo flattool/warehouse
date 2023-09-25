3def1c289d941ec8a95e43ab05941d18e3147f2a
heliguy4599 <heliguy4599@hotmail.com>
2023-09-25 17:24:47 -0400

    Bump version to 0.7.0

--- src/main.py
+++ src/main.py
@@ -92,7 +92,7 @@ class WarehouseApplication(Adw.Application):
             application_name="Warehouse",
             application_icon="io.github.flattool.Warehouse",
             developer_name="Heliguy",
-            version="0.6.0",
+            version="0.7.0",
             developers=["Heliguy https://github.com/heliguy4599", "kramo https://kramo.hu"],
             artists=["Heliguy https://github.com/heliguy4599", "kramo https://kramo.hu", "eryn https://github.com/hericiumvevo"],
             copyright='© 2023 The Files Authors',
