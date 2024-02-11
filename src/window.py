6be78a597f677c99dba4f46f6b072610a901d0d9
heliguy4599 <heliguy4599@hotmail.com>
2024-02-11 18:39:37 -0500

    Test commit

--- src/window.py
+++ src/window.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# SPDX-License-Identifier: GPL-3.0-only
+# SPDX-License-Identifier: GPL-3.0-only 
 
 import os
 import pathlib
