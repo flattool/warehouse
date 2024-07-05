fb308aa35af8f0483aeee78db3aabfa9b1829790
heliguy <heliguy4599@hotmail.com>
2024-07-05 03:13:17 -0400

    Further work on packages page

--- src/packages_page/packages_page.py
+++ src/packages_page/packages_page.py
@@ -1,6 +1,7 @@
-from gi.repository import Adw, Gtk, GLib, Gio, Pango
+from gi.repository import Adw, Gtk#, GLib, Gio, Pango
 from .host_info import HostInfo
 from .app_row import AppRow
+from .error_toast import ErrorToast
 
 @Gtk.Template(resource_path="/io/github/flattool/Warehouse/packages_page/packages_page.ui")
 class PackagesPage(Adw.BreakpointBin):
@@ -8,18 +9,16 @@ class PackagesPage(Adw.BreakpointBin):
     gtc = Gtk.Template.Child
     packages_list_box = gtc()
     sidebar_button = gtc()
-
-    package_rows = []
+    refresh_button = gtc()
+    packages_toast_overlay = gtc()
 
     def generate_list(self, *args):
+        self.packages_list_box.remove_all()
         for package in HostInfo.flatpaks:
             self.packages_list_box.append(AppRow(package))
 
     def row_select_handler(self, list_box, row):
         print(row.get_title())
-
-    def sidebar_button_visibility_handler(self):
-        self.sidebar_button.set_visible(self.main_window.main_split.get_collapsed() or not self.main_window.main_split.get_show_sidebar())
 
     def __init__(self, main_window, **kwargs):
         super().__init__(**kwargs)
@@ -28,4 +27,5 @@ class PackagesPage(Adw.BreakpointBin):
         self.sidebar_button.connect("clicked", lambda *_: main_window.main_split.set_show_sidebar(True))
         main_window.main_split.connect("notify::show-sidebar", lambda sidebar, *_: self.sidebar_button.set_visible(sidebar.get_collapsed() or not sidebar.get_show_sidebar()))
         main_window.main_split.connect("notify::collapsed", lambda sidebar, *_: self.sidebar_button.set_visible)
+        self.refresh_button.connect("clicked", lambda *_: HostInfo.get_flatpaks(self.generate_list))
         # self.packages_list_box.connect("row-selected", self.row_select_handler)
