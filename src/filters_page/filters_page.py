b68be309823e62f9d4df6095e734f24113b20cbd
heliguy <heliguy4599@hotmail.com>
2024-07-12 15:31:57 -0400

    Load all filters from gsettings into the UI

--- src/filters_page/filters_page.py
+++ src/filters_page/filters_page.py
@@ -22,11 +22,7 @@ class FiltersPage(Adw.NavigationPage):
     app_check = gtc()
     runtime_check = gtc()
     remotes_group = gtc()
-    remotes_search = gtc()
-    remotes_listbox = gtc()
     runtimes_group = gtc()
-    runtimes_search = gtc()
-    runtimes_listbox = gtc()
     all_remotes_button = gtc()
     all_runtimes_button = gtc()
 
@@ -34,11 +30,12 @@ class FiltersPage(Adw.NavigationPage):
     runtime_rows = []
 
     def generate_list(self):
-        self.remotes_search.set_text("")
-        self.runtimes_search.set_text("")
 
-        self.remotes_listbox.remove_all()
-        self.runtimes_listbox.remove_all()
+        for row in self.remote_rows:
+            self.remotes_group.remove(row)
+
+        for row in self.runtime_rows:
+            self.runtimes_group.remove(row)
 
         self.remote_rows.clear()
         self.runtime_rows.clear()
@@ -46,42 +43,40 @@ class FiltersPage(Adw.NavigationPage):
         self.app_check.set_active(self.settings.get_boolean("show-apps"))
         self.runtime_check.set_active(self.settings.get_boolean("show-runtimes"))
 
-        remotes_string = self.settings.get_string("runtimes-list")
-        self.all_remotes_button.set_active("all" != remotes_string)
+        remotes_string = self.settings.get_string("remotes-list")
         for i, installation in enumerate(HostInfo.installations):
             try:
                 for remote in HostInfo.remotes[installation]:
                     row = FilterRow(remote, installation)
                     row.set_title(remote.title)
                     row.set_subtitle(_("Installation: {}").format(installation))
+                    row.check_button.set_active(f"{remote.name}<>{installation}" in remotes_string)
+                    row.set_visible(False)
                     self.remote_rows.append(row)
-                    self.remotes_listbox.append(row)
+                    self.remotes_group.add(row)
             except KeyError:
                 pass
-        self.remotes_search.set_visible(i > 5)
+        self.all_remotes_button.set_active("all" != remotes_string)
 
         runtimes_string = self.settings.get_string("runtimes-list")
-        self.all_runtimes_button.set_active("all" != runtimes_string)
         for j, ref in enumerate(HostInfo.dependant_runtime_refs):
             row = FilterRow(ref)
             row.set_title(ref)
+            row.check_button.set_active(ref in runtimes_string)
+            row.set_visible(False)
             self.runtime_rows.append(row)
-            self.runtimes_listbox.append(row)
-        self.runtimes_search.set_visible(j > 5)
+            self.runtimes_group.add(row)
+        self.all_runtimes_button.set_active("all" != runtimes_string)
 
     def all_remotes_handler(self, switch, state):
-        self.remotes_group.set_description(_("Show packages from these remotes") if state else _("Showing packages from all remotes"))
+        for row in self.remote_rows:
+            row.set_visible(state)
+            row.set_sensitive(state)
 
     def all_runtimes_handler(self, switch, state):
-        self.runtimes_group.set_description(_("Show apps using these runtimes") if state else _("Showing packages using any runtime"))
-
-    def remotes_filter_func(self, row):
-        search_text = self.remotes_search.get_text().lower()
-        return search_text in row.get_title().lower()
-
-    def runtimes_filter_func(self, row):
-        search_text = self.runtimes_search.get_text().lower()
-        return search_text in row.get_title().lower()
+        for row in self.runtime_rows:
+            row.set_visible(state)
+            row.set_sensitive(state)
 
     def test(self, *args):
         print(self.settings.get_boolean("show-apps"))
@@ -96,13 +91,9 @@ class FiltersPage(Adw.NavigationPage):
         self.settings = Gio.Settings.new("io.github.flattool.Warehouse.filter")
 
         # Apply
-        self.remotes_listbox.set_filter_func(self.remotes_filter_func)
-        self.runtimes_listbox.set_filter_func(self.runtimes_filter_func)
 
         # Connections
         self.test_button.connect("clicked", self.test)
 
-        self.remotes_search.connect("search-changed", lambda *_: self.remotes_listbox.invalidate_filter())
-        self.runtimes_search.connect("search-changed", lambda *_: self.runtimes_listbox.invalidate_filter())
         self.all_remotes_button.connect("state-set", self.all_remotes_handler)
         self.all_runtimes_button.connect("state-set", self.all_runtimes_handler)
