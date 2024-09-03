fc521c6791bffc13b1d34f5a479ce74a8a328a8e
Heliguy <heliguy4599@hotmail.com>
2024-09-03 18:15:37 -0400

    Add ability to rename snapshots

--- src/snapshot_page/snapshot_box.py
+++ src/snapshot_page/snapshot_box.py
@@ -13,6 +13,9 @@ class SnapshotBox(Gtk.Box):
     version = gtc()
     apply_button = gtc()
     rename_button = gtc()
+    rename_menu = gtc()
+    rename_entry = gtc()
+    apply_rename = gtc()
     trash_button = gtc()
 
     def create_json(self):
@@ -24,14 +27,15 @@ class SnapshotBox(Gtk.Box):
             with open(self.json_path, 'w') as file:
                 json.dump(data, file, indent=4)
                 return None
+                
         except Exception as e:
             self.toast_overlay.add_toast(ErrorToast(_("Could not write data"), str(e)).toast)
 
-    def update_json(self):
+    def update_json(self, key, value):
         try:
             with open(self.json_path, 'r+') as file:
                 data = json.load(file)
-                data['name'] = "updated"
+                data[key] = value
                 file.seek(0)
                 json.dump(data, file, indent=4)
                 file.truncate()
@@ -39,12 +43,25 @@ class SnapshotBox(Gtk.Box):
         except Exception as e:
             self.toast_overlay.add_toast(ErrorToast(_("Could not write data"), str(e)).toast)
 
-    def json_handler(self, *args):
+    def load_from_json(self):
         if not os.path.exists(self.json_path):
             self.create_json()
+        
+        try:
+            with open(self.json_path, 'r') as file:
+                data = json.load(file)
+                name = data['name']
+                if name != "":
+                    self.title.set_label(GLib.markup_escape_text(name))
+                
+        except Exception as e:
+            self.toast_overlay.add_toast(ErrorToast(_("Could not write data"), str(e)).toast)
 
-        self.update_json()
-
+    def on_rename(self, widget):
+        self.update_json('name', self.rename_entry.get_text().strip())
+        self.load_from_json()
+        self.rename_menu.popdown()
+            
     def __init__(self, folder, snapshots_path, toast_overlay, **kwargs):
         super().__init__(**kwargs)
 
@@ -56,9 +73,8 @@ class SnapshotBox(Gtk.Box):
 
         date_data = GLib.DateTime.new_from_unix_local(int(split_folder[0])).format("%x %X")
         self.date.set_label(date_data)
-        self.version.set_label(split_folder[1].replace(".tar.zst", ""))
-        
+        self.version.set_label(_("Version: {}").format(split_folder[1].replace(".tar.zst", "")))
         self.json_path = f"{snapshots_path}{folder.replace('tar.zst', 'json')}"
-        
-        self.rename_button.connect("clicked", self.json_handler)
-        
+        self.load_from_json()
+        self.apply_rename.connect("clicked", self.on_rename)
+        self.rename_entry.connect("activate", self.on_rename)
