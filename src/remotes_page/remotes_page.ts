98c5f93ec93d462407f49f449ba53abb0535bb6a
heliguy4599 <heliguy4599@hotmail.com>
2026-01-29 16:17:26 -0500

    Show loading screen when loading remotes

--- src/remotes_page/remotes_page.ts
+++ src/remotes_page/remotes_page.ts
@@ -2,7 +2,7 @@ import Gtk from "gi://Gtk?version=4.0"
 import Adw from "gi://Adw?version=1"
 import Gio from "gi://Gio?version=2.0"
 
-import { GClass, Property, Child, from, OnSignal, next_idle, Debounce } from "../gobjectify/gobjectify.js"
+import { GClass, Property, Child, from, OnSignal, next_idle, Debounce, timeout_ms } from "../gobjectify/gobjectify.js"
 import { Installation, Remote } from "../flatpak.js"
 import { RemoteRow } from "./remote_row.js"
 import { BasePage } from "../widgets/base_page.js"
@@ -15,6 +15,7 @@ import "../widgets/search_group.js"
 export class RemotesPage extends from(BasePage, {
 	search_text: Property.string(),
 	show_disabled: Property.bool(),
+	is_loading: Property.bool(),
 	no_results: Property.bool(),
 	_search_filter: Child<Gtk.EveryFilter>(),
 	_enabled_filter: Child<Gtk.CustomFilter>(),
@@ -23,7 +24,7 @@ export class RemotesPage extends from(BasePage, {
 	_map_model: Child<Gtk.MapListModel<Gio.ListStore<Remote>>>(),
 	_current_group: Child<Adw.PreferencesGroup>(),
 }) implements BasePage {
-	_ready(): void {
+	async _ready(): Promise<void> {
 		this._only_remotes_filter.set_filter_func((item) => item instanceof Remote)
 		this._map_model.set_map_func((item) => {
 			if (!(item instanceof Installation)) return item
@@ -35,6 +36,10 @@ export class RemotesPage extends from(BasePage, {
 			if (this.show_disabled) return true
 			return !remote.disabled
 		})
+		await timeout_ms(250)
+		if (this._remotes_list.get_n_items() === 0) {
+			this.is_loading = false
+		}
 	}
 
 	@OnSignal("notify::show-disabled")
@@ -57,9 +62,14 @@ export class RemotesPage extends from(BasePage, {
 		this.no_results = !any_results
 	}
 
+	@Debounce(200, { trigger: "leading" })
+	protected _on_list_change_start(): void {
+		this.is_loading = true
+	}
+
 	@Debounce(200)
-	protected _on_list_changed(): void {
-		this.#do_search()
+	protected _on_list_change_finish(): void {
+		this.#do_search().then(() => this.is_loading = false)
 	}
 
 	protected _on_search_changed(entry: Gtk.SearchEntry): void {
