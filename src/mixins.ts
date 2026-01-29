77dd67c4968a5d1fc7d8ba1ed0daae3bbcf51ad7
heliguy4599 <heliguy4599@hotmail.com>
2026-01-29 01:53:25 -0500

    add extension method for strings to normalize file paths

--- src/mixins.ts
+++ src/mixins.ts
@@ -3,9 +3,18 @@ import GLib from "gi://GLib?version=2.0"
 declare global {
 	interface String {
 		markup_escape_text(this: string): string
+		normalize_path(this: string): string
 	}
 }
 
 String.prototype.markup_escape_text = function (): string {
 	return GLib.markup_escape_text(this, -1)
+}
+
+String.prototype.normalize_path = function (): string {
+	let normalized = this.replace(/\/+/g, "/")
+	if (normalized.length > 1 && normalized.endsWith("/")) {
+		normalized = normalized.slice(0, -1)
+	}
+	return normalized
 }
