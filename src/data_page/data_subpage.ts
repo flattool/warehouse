dce8e1335caf3358d09e96ab1865b9d06f65996c
heliguy4599 <heliguy4599@hotmail.com>
2026-02-08 22:34:38 -0500

    UI work again

--- src/data_page/data_subpage.ts
+++ src/data_page/data_subpage.ts
@@ -1,8 +1,13 @@
+import Gtk from "gi://Gtk?version=4.0"
 import Adw from "gi://Adw?version=1"
 
 import { GClass, Property, from } from "../gobjectify/gobjectify.js"
 
 @GClass({ template: "resource:///io/github/flattool/Warehouse/data_page/data_subpage.ui" })
-export class DataSubpage extends from(Adw.Bin, {
+export class DataSubpage extends from(Adw.BreakpointBin, {
 	show_leftover: Property.bool(),
-}) {}
+}) {
+	protected _get_title(__: this, show_leftover: boolean): string {
+		return show_leftover ? _("Leftover Data") : _("Active Data")
+	}
+}
