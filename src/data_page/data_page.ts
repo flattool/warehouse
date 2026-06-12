6526d38df9d6ec5f7d754942809d1ce1e8199204
heliguy4599 <heliguy4599@hotmail.com>
2026-06-11 20:07:55 -0400

    Implement initial DataPage information loading

--- src/data_page/data_page.ts
+++ src/data_page/data_page.ts
@@ -1,18 +1,65 @@
 import Adw from "gi://Adw?version=1"
+import Gtk from "gi://Gtk?version=4.0"
+import Gio from "gi://Gio?version=2.0"
 
-import { GClass, Property, from } from "../gobjectify/gobjectify.js"
+import { Child, GClass, OnSimpleAction, Property, SimpleAction, WatchProp, from } from "../gobjectify/gobjectify.js"
 import { BasePage } from "../widgets/base_page.js"
+import { DataSubpage } from "./data_subpage.js"
+import { Package } from "../flatpak.js"
+import { FileList } from "../utils/file_list.js"
 
 import "../widgets/sidebar_button.js"
 import "./data_subpage.js"
 import "../widgets/search_button.js"
+import "../widgets/select_button.js"
+import { iterate_list_model } from "../utils/helper_funcs.js"
 
 @GClass({ template: "resource:///io/github/flattool/Warehouse/data_page/data_page.ui" })
-export class DataPage extends from(BasePage, {}) {
+export class DataPage extends from(BasePage, {
+	selection_mode_enabled: Property.readwrite.bool(),
+	data_dir: Property.readonly.gobject(Gio.File),
+	request_selection_mode: SimpleAction(),
+	_active_data: Child<Gio.ListModel<Gio.File>>(),
+	_active_filter: Child<Gtk.CustomFilter>(),
+	_leftover_data: Child<Gio.ListModel<Gio.File>>(),
+	_leftover_filter: Child<Gtk.CustomFilter>(),
+	_current: Child<DataSubpage>(),
+	_leftover: Child<DataSubpage>(),
+}) {
+	readonly #seen_paths = new Set<string>()
+
 	constructor(params?: typeof DataPage.$params) {
 		params ??= {}
 		params.icon_name = "warehouse:file-manager-symbolic"
 		params.sidebar_title = _("User Data")
+		params.data_dir = Package.user_data_dir
 		super(params)
+		this._active_filter.set_filter_func((item) => {
+			if (!(item instanceof Gio.File) || !this.installations) return false
+			for (const inst of iterate_list_model(this.installations)) {
+				if (inst.has_package_by_id(item.get_basename()!)) return true
+			}
+			return false
+		})
+		this._leftover_filter.set_filter_func((item) => {
+			if (!(item instanceof Gio.File) || !this.installations) return false
+			for (const inst of iterate_list_model(this.installations)) {
+				if (inst.has_package_by_id(item.get_basename()!)) return false
+			}
+			return true
+		})
+	}
+
+	@OnSimpleAction("request_selection_mode")
+	#on_request_selection_mode(): void {
+		this.selection_mode_enabled = true
+	}
+
+	@WatchProp("loading")
+	#on_loading_changed(): void {
+		if (this.loading) {
+			this.#seen_paths.clear()
+		}
+		this.selection_mode_enabled = false
 	}
 }
--- src/data_page/data_subpage.ts
+++ src/data_page/data_subpage.ts
@@ -1,13 +1,41 @@
 import Gtk from "gi://Gtk?version=4.0"
 import Adw from "gi://Adw?version=1"
+import Gio from "gi://Gio?version=2.0"
 
-import { GClass, Property, from } from "../gobjectify/gobjectify.js"
+import { Child, GClass, Property, WatchProp, from, next_idle } from "../gobjectify/gobjectify.js"
+import { DataBox } from "./data_box.js"
 
 @GClass({ template: "resource:///io/github/flattool/Warehouse/data_page/data_subpage.ui" })
 export class DataSubpage extends from(Adw.BreakpointBin, {
 	show_leftover: Property.readonly.bool(),
+	selection_mode_enabled: Property.readwrite.bool(),
+	folders: Property.readonly.gobject(Gio.ListModel).as<Gio.ListModel<Gio.File>>(),
+	_flow_box: Child<Gtk.FlowBox>(),
 }) {
+	constructor(params?: typeof DataSubpage.$params) {
+		super(params)
+		if (this.folders) {
+			this._flow_box.bind_model(this.folders, (folder) => {
+				const box = new DataBox({ folder })
+				next_idle().then(() => box.parent.focusable = false)
+				return box
+			})
+		}
+	}
+
 	protected _get_title(__: this, show_leftover: boolean): string {
 		return show_leftover ? _("Leftover Data") : _("Active Data")
+	}
+
+	on_button_clicked(): void {
+		print("button clicked")
+	}
+
+	@WatchProp("selection_mode_enabled")
+	#on_selection_mode_changed(): void {
+		for (const child of this._flow_box) {
+			if (!(child instanceof Gtk.FlowBoxChild) || !(child.child instanceof DataBox)) continue
+			child.child.selection_mode_enabled = this.selection_mode_enabled
+		}
 	}
 }
