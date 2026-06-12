1a3dd6f6e1b0e6cecdc15c029f63634805b16b2f
heliguy4599 <heliguy4599@hotmail.com>
2026-06-11 23:06:22 -0400

    Fix SidebarButton

--- src/widgets/sidebar_button.ts
+++ src/widgets/sidebar_button.ts
@@ -1,29 +1,26 @@
 import Gtk from "gi://Gtk?version=4.0"
-import type Adw from "gi://Adw?version=1"
 
 import { SharedVars } from "../utils/shared_vars.js"
-import { GClass, OnSignal, PostInit, from } from "../gobjectify/gobjectify.js"
+import { GClass, OnSignal, PostInit } from "../gobjectify/gobjectify.js"
 
 @GClass()
-export class SidebarButton extends from(Gtk.Button, {}) {
-	split_view?: Adw.OverlaySplitView = SharedVars.main_window?._split_view
-
-	constructor(params?: typeof SidebarButton.$params) {
-		super(params)
+export class SidebarButton extends Gtk.Button {
+	@PostInit
+	#ready(): void {
 		this.icon_name = (this.get_direction() === Gtk.TextDirection.RTL
 			? "warehouse:sidebar-show-right-symbolic"
 			: "warehouse:sidebar-show-symbolic"
 		)
-		this.split_view?.$connect("notify::collapsed", () => this.#on_collapse())
+		SharedVars.main_window?._split_view.$connect("notify::collapsed", () => this.#on_collapse())
 	}
 
 	@PostInit
 	#on_collapse(): void {
-		this.set_visible(this.split_view?.collapsed ?? false)
+		this.set_visible(SharedVars.main_window?._split_view?.collapsed ?? false)
 	}
 
 	@OnSignal("clicked")
 	#on_clicked(): void {
-		this.split_view?.set_show_sidebar(true)
+		SharedVars.main_window?._split_view?.set_show_sidebar(true)
 	}
 }
