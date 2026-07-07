7f5343ac8fedfb872fefef92fbb8de76d9a978d6
heliguy4599 <heliguy4599@hotmail.com>
2026-07-06 23:33:01 -0400

    Fix run script for flake environment

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
-flatpak run org.flatpak.Builder --install --user --force-clean _build build-aux/io.github.flattool.Warehouse.json \
-&& flatpak run io.github.flattool.Warehouse//master
+set -euo pipefail
+
+# Needed to escape weird envvars provided mainly by Nix flakes
+flatpak run \
+	--unset-env=GDK_PIXBUF_MODULE_FILE \
+	--unset-env=GIO_EXTRA_MODULES \
+	--unset-env=GI_TYPELIB_PATH \
+	org.flatpak.Builder --install --user --force-clean _build build-aux/io.github.flattool.Warehouse.json
+
+flatpak run io.github.flattool.Warehouse//master
