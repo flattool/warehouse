ce4a996165a7fb3b810bf4111ec5b2203ba01254
heliguy4599 <heliguy4599@hotmail.com>
2026-07-08 23:49:14 -0400

    Implement sorting

--- src/data_page/data_page.ts
+++ src/data_page/data_page.ts
@@ -23,6 +23,7 @@ import "../widgets/sidebar_button.js"
 import "./data_subpage.js"
 import "../widgets/search_button.js"
 import "../widgets/select_button.js"
+import GLib from "gi://GLib?version=2.0"
 
 type SortKind = "name" | "id" | "size"
 type OrderKind = "asc" | "desc"
@@ -32,7 +33,7 @@ const make_sort_menu = (): Gio.Menu => Menu.build(
 		_("Sort"),
 		Menu.item_group(
 			DataPage,
-			"sort",
+			"sort_act",
 			{ label: _("Name"), target: "name" },
 			{ label: _("ID"), target: "id" },
 			{ label: _("Size"), target: "size" },
@@ -42,7 +43,7 @@ const make_sort_menu = (): Gio.Menu => Menu.build(
 		null,
 		Menu.item_group(
 			DataPage,
-			"order",
+			"order_act",
 			{ label: _("Ascending"), target: "asc" },
 			{ label: _("Descending"), target: "desc" },
 		),
@@ -54,9 +55,12 @@ export class DataPage extends from(BasePage, {
 	selection_mode_enabled: Property.readwrite.bool(),
 	data_dir: Property.readonly.gobject(Gio.File),
 	file_sorter: Property.readwrite.gobject(Gtk.Sorter),
+	sorter: Property.readwrite.gobject(Gtk.CustomSorter),
+	sort: Property.readwrite.string("name").as<SortKind>(),
+	order: Property.readwrite.string("asc").as<OrderKind>(),
+	sort_act: SimplerAction.property("sort", (s: SortKind) => GLib.Variant.new_string(s)),
+	order_act: SimplerAction.property("order", (o: OrderKind) => GLib.Variant.new_string(o)),
 	request_selection_mode: SimplerAction.void(),
-	sort: SimplerAction.state.string({ default: "name" }).as<SortKind>(),
-	order: SimplerAction.state.string({ default: "asc" }).as<OrderKind>(),
 	_active_data: Child<ArrayStore<Gio.File>>(),
 	_all_packages: Child<Gtk.FlattenListModel<Package>>(),
 	_installations_packages: Child<Gtk.MapListModel>(),
@@ -66,9 +70,7 @@ export class DataPage extends from(BasePage, {
 	_current: Child<DataSubpage>(),
 	_leftover: Child<DataSubpage>(),
 }) {
-	#ascending = true
-
-	#id_sorter = Gtk.CustomSorter.new((one, two) => {
+	readonly #id_sorter = Gtk.CustomSorter.new((one, two) => {
 		if (one === two) return 0
 		if (!(one instanceof Gio.File)) return -1
 		if (!(two instanceof Gio.File)) return 1
@@ -77,10 +79,10 @@ export class DataPage extends from(BasePage, {
 		).localeCompare(
 			two.get_basename() ?? "",
 		)
-		return this.#ascending ? result : -result
+		return this.order === "asc" ? result : -result
 	})
 
-	#name_sorter = Gtk.CustomSorter.new((one, two) => {
+	readonly #name_sorter = Gtk.CustomSorter.new((one, two) => {
 		if (one === two) return 0
 		if (!(one instanceof Gio.File)) return -1
 		if (!(two instanceof Gio.File)) return 1
@@ -89,10 +91,21 @@ export class DataPage extends from(BasePage, {
 		).localeCompare(
 			two.get_basename()?.split(".").at(-1) ?? "",
 		)
-		return this.#ascending ? result : -result
+		return this.order === "asc" ? result : -result
 	})
 
-	#package_ids = new Set<string>()
+	readonly #size_sorter = Gtk.CustomSorter.new((one, two) => {
+		if (one === two) return 0
+		if (!(one instanceof Gio.File)) return -1
+		if (!(two instanceof Gio.File)) return 1
+		const size_one = this.#size_cache.get(one) ?? -1
+		const size_two = this.#size_cache.get(two) ?? -1
+		const result = size_one - size_two
+		return this.order === "asc" ? result : -result
+	})
+
+	readonly #package_ids = new Set<string>()
+	readonly #size_cache = new Map<Gio.File, number>()
 
 	constructor(params?: typeof DataPage.$params) {
 		params ??= {}
@@ -105,13 +118,44 @@ export class DataPage extends from(BasePage, {
 		this._all_packages.connect("items-changed", () => this.#rebuild_package_ids())
 		this._files.connect("items-changed", () => this.#rebuild_package_ids())
 		this._sort_button.menu_model = make_sort_menu()
+		this._current.$connect("folder-size-reported", this.#on_folder_size_reported.bind(this))
+		this._leftover.$connect("folder-size-reported", this.#on_folder_size_reported.bind(this))
+	}
+
+	#on_folder_size_reported(_page: unknown, folder: Gio.File | null, size: number): void {
+		if (!folder) return
+		this.#size_cache.set(folder, size)
+		if (this.sort === "size") {
+			this.#update_sorter()
+		}
+	}
+
+	@WatchProp("order")
+	@Debounce(200)
+	#update_sorter(): void {
+		this.selection_mode_enabled = false
+		this.sorter?.changed(Gtk.SorterChange.DIFFERENT)
+	}
+
+	@WatchProp("sort")
+	#on_sort_changed(): void {
+		this.selection_mode_enabled = false
+		this.sorter = ((): Gtk.CustomSorter => {
+			switch (this.sort) {
+				case "id": return this.#id_sorter
+				case "name": return this.#name_sorter
+				case "size": return this.#size_sorter
+			}
+		})()
 	}
 
 	@Debounce(200)
 	#rebuild_package_ids(): void {
+		this.#package_ids.clear()
+		this.#size_cache.clear()
+
 		const seen_active_ids = new Set<string>()
 		const active_dirs: Gio.File[] = []
-		this.#package_ids.clear()
 		for (const pkg of iterate_list_model(this._all_packages)) {
 			this.#package_ids.add(pkg.application)
 			if (!pkg.is_app || seen_active_ids.has(pkg.application) || !pkg.data_dir?.query_exists(null)) {
--- src/data_page/data_subpage.ts
+++ src/data_page/data_subpage.ts
@@ -2,7 +2,7 @@ import Gtk from "gi://Gtk?version=4.0"
 import Adw from "gi://Adw?version=1"
 import Gio from "gi://Gio?version=2.0"
 
-import { Child, GClass, PostInit, Property, WatchProp, from, next_idle } from "../gobjectify/gobjectify.js"
+import { Child, GClass, PostInit, Property, Signal, WatchProp, from, next_idle } from "../gobjectify/gobjectify.js"
 import { DataBox } from "./data_box.js"
 import { get_readable_byte_size, iterate_list_model } from "../utils/helper_funcs.js"
 
@@ -32,6 +32,7 @@ class SizeRecorder {
 
 @GClass({ template: "resource:///io/github/flattool/Warehouse/data_page/data_subpage.ui" })
 export class DataSubpage extends from(Adw.BreakpointBin, {
+	folder_size_reported: Signal([Gio.File, Number]),
 	show_leftover: Property.readonly.bool(),
 	selection_mode_enabled: Property.readwrite.bool(),
 	folders: Property.readonly.gobject(Gio.ListModel).as<Gio.ListModel<Gio.File>>(),
@@ -52,7 +53,10 @@ export class DataSubpage extends from(Adw.BreakpointBin, {
 		this.folders.connect("items-changed", () => this.#begin_size_load())
 		this._flow_box.bind_model(this.folders, (folder) => {
 			const box = new DataBox({ folder, is_leftover: this.show_leftover })
-			box.$connect("size-reported", (__, size) => this.#size_recorder.add(size))
+			box.$connect("size-reported", (__, size) => {
+				this.#size_recorder.add(size)
+				this.$emit("folder-size-reported", folder, size)
+			})
 			box.$connect("notify::is-selected", () => {
 				if (box.is_selected) {
 					this.#selected_folders.add(folder)
