fa432bd65e3dc1ea7ba9076bc5362fdf4701ea8e
heliguy4599 <heliguy4599@hotmail.com>
2026-07-18 02:10:14 -0400

    Make remote dropdown for search page

--- src/install_page/select_page.ts
+++ src/install_page/select_page.ts
@@ -7,42 +7,32 @@ import { Child, from, GClass, Property, WatchProp } from "../gobjectify/gobjecti
 import { Installation, Remote } from "../flatpak.js"
 import { ArrayStore } from "../utils/array_store.js"
 import { make_signal_factory } from "../utils/helper_funcs.js"
+import { SelectableRemote, SelectableRemoteBox } from "./selectable_remote.js"
 
 @GClass({ template: "resource:///io/github/flattool/Warehouse/install_page/select_page.ui" })
 export class SelectPage extends from(Adw.Bin, {
 	remotes: Property.readwrite.gobject(Gio.ListModel).as<Gio.ListModel<Remote>>(),
+	selected_remote: Property.readwrite.gobject(SelectableRemote),
+	_selectable_remotes: Child<Gtk.MapListModel<SelectableRemote>>(),
 	_remote_dropdown: Child<Gtk.DropDown>(),
 }) {
 	constructor(params?: typeof SelectPage.$params) {
 		super(params)
-		this._remote_dropdown.list_factory = make_signal_factory(FilterableRemote, Remote, {
-			setup: () => new FilterableRemote(),
-			bind: (filterable, remote) => filterable.remote = remote,
-			unbind: (filterable) => filterable.remote = null,
+		this._selectable_remotes.set_map_func((item) => new SelectableRemote({ remote: item as Remote }))
+		this._remote_dropdown.list_factory = make_signal_factory(SelectableRemoteBox, SelectableRemote, {
+			setup: () => new SelectableRemoteBox(),
+			bind: (box, remote) => box.remote = remote,
+			unbind: (box) => box.remote = null,
 		})
 	}
-}
 
-@GClass() class FilterableRemote extends from(Gtk.Box, {
-	remote: Property.readwrite.gobject(Remote),
-}) {
-	#title_label = new Gtk.Label({ hexpand: true, halign: Gtk.Align.START })
-	#inst_label = new Gtk.Label({ hexpand: true, halign: Gtk.Align.START })
-	#name_label = new Gtk.Label({ hexpand: true, halign: Gtk.Align.END })
-
-	constructor(params?: typeof FilterableRemote.$params) {
-		super(params)
-		const left_box = new Gtk.Box({ hexpand: true, orientation: Gtk.Orientation.VERTICAL })
-		left_box.append(this.#title_label)
-		left_box.append(this.#inst_label)
-		this.append(left_box)
-		this.append(this.#name_label)
-	}
-
-	@WatchProp("remote")
-	#on_remote_changed(): void {
-		this.#title_label.label = this.remote?.title ?? ""
-		this.#inst_label.label = this.remote?.installation?.title ?? ""
-		this.#name_label.label = this.remote?.name ?? ""
+	protected _remote_selected(): void {
+		if (this.selected_remote) {
+			this.selected_remote.selected = false
+		}
+		this.selected_remote = this._remote_dropdown.selected_item as SelectableRemote | null
+		if (this.selected_remote) {
+			this.selected_remote.selected = true
+		}
 	}
 }
---
+++ src/install_page/selectable_remote.ts
@@ -0,0 +1,33 @@
+import GObject from "gi://GObject?version=2.0"
+
+import { from, GClass, Property, WatchProp } from "../gobjectify/gobjectify.js"
+import { Remote } from "../flatpak.js"
+import Gtk from "gi://Gtk?version=4.0"
+
+@GClass() export class SelectableRemote extends from(GObject.Object, {
+	remote: Property.readwrite.gobject(Remote),
+	selected: Property.readwrite.bool(),
+	is_all: Property.readwrite.bool(),
+}) {}
+
+@GClass({ template: "resource:///io/github/flattool/Warehouse/install_page/selectable_remote_box.ui" })
+export class SelectableRemoteBox extends from(Gtk.Box, {
+	selectable_remote: Property.readwrite.gobject(SelectableRemote),
+	remote: Property.readwrite.gobject(SelectableRemote),
+}) {
+	protected _get_check_opacity(__: this, is_selected: boolean): number {
+		return is_selected ? 1 : 0
+	}
+
+	protected _get_title_label(__: this, s_remote: SelectableRemote | null): string {
+		return s_remote?.remote?.title ?? ""
+	}
+
+	protected _get_name_label(__: this, s_remote: SelectableRemote | null): string {
+		return s_remote?.remote?.name ?? ""
+	}
+
+	protected _get_installation_label(__: this, s_remote: SelectableRemote | null): string {
+		return _("Installation: %s").format(s_remote?.remote?.installation?.title ?? "")
+	}
+}
