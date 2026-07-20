bc4be0f18bee53fb41bb98a09a83291b7cd85258
heliguy4599 <heliguy4599@hotmail.com>
2026-07-20 15:48:38 -0400

    Implement searching all remotes and insts

--- src/install_page/select_page.ts
+++ src/install_page/select_page.ts
@@ -3,25 +3,27 @@ import Gtk from "gi://Gtk?version=4.0"
 import Gio from "gi://Gio?version=2.0"
 
 import { Child, from, GClass, Property, WatchProp } from "../gobjectify/gobjectify.js"
-import { Remote, search_packages, SearchResult } from "../flatpak.js"
+import { Installation, Remote, search_packages, SearchResult } from "../flatpak.js"
 import { ArrayStore } from "../utils/array_store.js"
-import { make_signal_factory } from "../utils/helper_funcs.js"
+import { iterate_list_model, make_signal_factory } from "../utils/helper_funcs.js"
 import { SelectableRemote, SelectableRemoteBox } from "./selectable_remote.js"
 import { ResultRow } from "./result_row.js"
 import { SwitchTask } from "../utils/switch_task.js"
 
+import "../widgets/sidebar_button.js"
 import "../widgets/loading_group.js"
 
 type StackPages = "empty-search" | "results-list" | "no-results"
 
 @GClass({ template: "resource:///io/github/flattool/Warehouse/install_page/select_page.ui" })
 export class SelectPage extends from(Adw.Bin, {
+	installations: Property.readwrite.gobject(Gio.ListModel).as<Gio.ListModel<Installation>>(),
 	remotes: Property.readwrite.gobject(Gio.ListModel).as<Gio.ListModel<Remote>>(),
 	selected_remote: Property.readwrite.gobject(SelectableRemote),
 	search_results: Property.readwrite.gobject(ArrayStore<SearchResult>),
 	is_searching: Property.readwrite.bool(),
 	visible_stack_page: Property.readwrite.string("empty-search").as<StackPages>(),
-	_selectable_remotes: Child<Gtk.MapListModel<SelectableRemote>>(),
+	_available_remotes: Child<Gtk.MapListModel<SelectableRemote>>(),
 	_sorted_results: Child<Gtk.SortListModel<SearchResult>>(),
 	_search_entry: Child<Gtk.SearchEntry>(),
 	_remote_dropdown: Child<Gtk.DropDown>(),
@@ -31,13 +33,14 @@ export class SelectPage extends from(Adw.Bin, {
 
 	constructor(params?: typeof SelectPage.$params) {
 		super(params)
-		this._selectable_remotes.set_map_func((item) => new SelectableRemote({ remote: item as Remote }))
+		this._available_remotes.set_map_func((item) => new SelectableRemote({ remote: item as Remote }))
 		this._remote_dropdown.list_factory = make_signal_factory(SelectableRemoteBox, SelectableRemote, {
 			setup: () => new SelectableRemoteBox(),
-			bind: (box, remote) => box.remote = remote,
-			unbind: (box) => box.remote = null,
+			bind: (box, s_remote) => box.selectable_remote = s_remote,
+			unbind: (box) => box.selectable_remote = null,
 		})
 		this._results_box.bind_model(this._sorted_results, (item) => new ResultRow({ result: item as SearchResult }))
+		this._remote_selected()
 	}
 
 	@WatchProp("selected_remote")
@@ -45,21 +48,33 @@ export class SelectPage extends from(Adw.Bin, {
 		this.search_results = new ArrayStore()
 
 		const search = this._search_entry.get_text().trim()
-		const inst = this.selected_remote?.remote?.installation
-		if (!inst || !search || search.length < 4) {
+		if (!search || search.length < 4) {
 			this.#search_task.cancel()
 			this.visible_stack_page = "empty-search"
 			return
 		}
 
-		const remote = this.selected_remote?.remote
-		const results = await this.#search_task.run(
-			(cancellable) => search_packages(search, inst, remote?.name, cancellable),
-		).catch(
-			(e) => print("Error when searching:", e),
-		)
+		const selected_inst = this.selected_remote?.remote?.installation
+		let insts: IterableIterator<Installation>
+		if (selected_inst) {
+			insts = [selected_inst][Symbol.iterator]()
+		} else if (this.installations) {
+			insts = iterate_list_model(this.installations)
+		} else {
+			insts = [][Symbol.iterator]()
+		}
 
-		if (!results) return
+		let results: SearchResult[] = []
+		for (const inst of insts) {
+			const inst_result = await this.#search_task.run(
+				(cancellable) => search_packages(search, inst, this.selected_remote?.remote?.name, cancellable),
+			).catch(
+				(e) => print("Error when searching:", e),
+			)
+			if (!inst_result || inst_result.length < 1) continue
+			results.push(...inst_result)
+		}
+
 		this.search_results.swap_contents(results)
 		this.visible_stack_page = results.length > 0 ? "results-list" : "no-results"
 	}
--- src/install_page/selectable_remote.ts
+++ src/install_page/selectable_remote.ts
@@ -5,10 +5,17 @@ import { Remote } from "../flatpak.js"
 import Gtk from "gi://Gtk?version=4.0"
 
 @GClass() export class SelectableRemote extends from(GObject.Object, {
-	remote: Property.readwrite.gobject(Remote),
+	remote: Property.readonly.gobject(Remote),
 	selected: Property.readwrite.bool(),
 	is_all: Property.readwrite.bool(),
+	title: Property.readonly.string(),
 }) {
+	constructor(params: typeof SelectableRemote.$params) {
+		params ??= {}
+		params.title = params.is_all ? _("All Remotes") : params.remote?.title ?? ""
+		super(params)
+	}
+
 	set_selected(to: boolean): void {
 		this.selected = to
 	}
@@ -17,21 +24,27 @@ import Gtk from "gi://Gtk?version=4.0"
 @GClass({ template: "resource:///io/github/flattool/Warehouse/install_page/selectable_remote_box.ui" })
 export class SelectableRemoteBox extends from(Gtk.Box, {
 	selectable_remote: Property.readwrite.gobject(SelectableRemote),
-	remote: Property.readwrite.gobject(SelectableRemote),
 }) {
 	protected _get_check_opacity(__: this, is_selected: boolean): number {
 		return is_selected ? 1 : 0
 	}
 
 	protected _get_title_label(__: this, s_remote: SelectableRemote | null): string {
-		return s_remote?.remote?.title ?? ""
+		return s_remote?.title ?? ""
 	}
 
 	protected _get_name_label(__: this, s_remote: SelectableRemote | null): string {
 		return s_remote?.remote?.name ?? ""
 	}
 
+	protected _get_name_label_visible(__: this, s_remote: SelectableRemote | null): boolean {
+		return !s_remote?.is_all
+	}
+
 	protected _get_installation_label(__: this, s_remote: SelectableRemote | null): string {
-		return _("Installation: %s").format(s_remote?.remote?.installation?.title ?? "")
+		return (s_remote?.is_all
+			? _("Installation: Any")
+			: _("Installation: %s").format(s_remote?.remote?.installation?.title ?? "")
+		)
 	}
 }
